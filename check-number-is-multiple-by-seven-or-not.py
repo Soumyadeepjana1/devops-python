356ae94a33e8f0b4eb63cb555a7bf12ee6c2fa79
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-23 21:26:21 +0530

    feat: add scripts for checking divisibility by seven and finding the largest of three numbers

---
+++ check-number-is-multiple-by-seven-or-not.py
@@ -0,0 +1,6 @@
+a = int(input("enter a number: "))
+
+if a % 7 == 0:
+    print("number is divisible by 7")
+else:
+    print("the number is not divisible by 7")
---
+++ find-largest-num.py
@@ -0,0 +1,11 @@
+a = 22
+b = 21
+c = 10
+
+
+if (a> b> c):
+    print("a is larger")
+elif (b > a > c):
+    print("b is larger")
+else:
+    print("c is larger ")
