3fb08fdc9d08f04db392cd886ce5087262453b31
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-23 21:17:05 +0530

    learn nested condition

---
+++ nested-loop.py
@@ -0,0 +1,9 @@
+age = int(input("enter your age: "))
+
+if (age >= 18):
+    if age >= 80:
+        print("you can eligible for senior citizen discount")
+    else:
+        print("you can eligible for vooting")
+else:
+    print("you are not eligible for votting")
