184cc48353cf1712ad4d29de38dfdc60e86f62a9
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-24 20:21:48 +0530

    adding all

---
+++ devops-python/nestedloop.py
@@ -0,0 +1,8 @@
+name = str((input("enter your name: ")))
+age = int(input("enter your age: "))
+
+if (name == "soumyadeep"):
+    if(age == 21):
+        print("person is le")
+else:
+    print("he is not soumyadeep ")
