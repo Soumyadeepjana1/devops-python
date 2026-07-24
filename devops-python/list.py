71350ca94f01bd0eda4f564ded2fdf857be529a8
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-24 20:40:17 +0530

    done

---
+++ devops-python/list.py
@@ -0,0 +1,20 @@
+age = [12,13,161,17]
+print(len(age))
+print(age[12:16])
+age[0] = 16
+print(age)
+print(age[0:2]) #list sliyacing
+
+age.append(19) # append funcation help to add one eliment in list
+print(age)
+
+age.sort()  #help to sort small to big
+
+
+age.sort(reverse=True) # help to sort big to small
+
+age.reverse() #help to print reverse element
+
+age.insert(3,15) #insert value (index,element)
+
+print(age)
