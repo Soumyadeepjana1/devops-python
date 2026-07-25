3b5b164b182c1664bd2213f843622c74f55be03d
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-25 22:08:15 +0530

    add

--- devops-python/set-practice.py
+++ devops-python/set-practice.py
@@ -1,7 +1,52 @@
-set1 = {"hello", " boss" , "king" , "don"}
+# set1 = {"hello", " boss" , "king" , "don"}
 
-set2 = {"king" , "don", "soumyadeep" , "cobra"}
+# set2 = {"king" , "don", "soumyadeep" , "cobra"}
 
-print(set1.union(set2)) #help to make a new set to combining 2 set ex like set1 and set 2
+# print(set1.union(set2)) #help to make a new set to combining 2 set ex like set1 and set 2
 
-print(set1.intersection(set2)) # help to make a new set those are common 
+# print(set1.intersection(set2)) # help to make a new set those are common 
+
+
+
+
+# animal = {
+    
+#     "table" : ["a pice of ferniture","list of fact and figure"],
+#     "cat" : "a small animale"
+# }
+
+# print(animal["table"])
+# print(animal["cat"])
+
+
+# classroom = {"python" , "java" , "c++" , "python" , "javascript"}
+# classrooms = {"java" , "python" , "java" , "c++" , "c" }
+
+
+# room = classroom.union(classrooms) #  this is right
+
+# # room = list(classroom.intersection(classrooms))
+# print(room)
+# print(len(room))
+
+
+
+# subject1 = str(input("enter subject name: "))
+# mark1 = int(input("enter yours marks"))
+# subject2 = str(input("enter 2 nd subject name"))
+# mark2 = int(input("enter your marks: "))
+# subject3 = str(input("enter 3 d subject : "))
+
+# dic = {}
+# dic[subject1] = mark1
+# dic[subject2] = mark2
+
+# print(dic)
+# print(type(dic))
+
+
+
+sru = {9 , "9.0"}
+
+print(sru)
+
