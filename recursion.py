599379ee2f98f8e78aeda5e3055c8cad4af62f45
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-29 20:41:52 +0530

    add all

--- recursion.py
+++ recursion.py
@@ -1,46 +1,76 @@
-def show(n):
-    if n >10:
-        return
-    print(n)
-    show(n + 1)
+# def show(n):
+#     if n >10:
+#         return
+#     print(n)
+#     show(n + 1)
     
-show(5)
+# show(5)
 
 
-def show(n):
-    if n > 10:
-        return
-    print(n)
-    show(n + 1)
+# def show(n):
+#     if n > 10:
+#         return
+#     print(n)
+#     show(n + 1)
 
-show(5)
+# show(5)
 
-def king(j):
-    if  j == 1:
-        return
-    print(j)
-    king(j-1)
-king(5)
-
-
-def jod(j):
-    if j == 10:
-        return
-    print(j)
-    jod (j +1)
-jod(1)
-
-def cobra(n):
-    if n <= 10:
-        return
-    print(n)
-    cobra(n -1)
-cobra(20)
+# def king(j):
+#     if  j == 1:
+#         return
+#     print(j)
+#     king(j-1)
+# king(5)
 
 
-def dog(n):
-    if n > 100:
-        return
-    print(n)
-    dog( n + 1)
-dog(1)
+# def jod(j):
+#     if j == 10:
+#         return
+#     print(j)
+#     jod (j +1)
+# jod(1)
+
+# def cobra(n):
+#     if n <= 10:
+#         return
+#     print(n)
+#     cobra(n -1)
+# cobra(20)
+
+
+# def dog(n):
+#     if n > 100:
+#         return
+#     print(n)
+#     dog( n + 1)
+# dog(1)
+
+
+
+
+# def fac(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return fac(n - 5) * n
+# print(fac(10))
+
+
+# def odd_even(n):
+#     if n == 0:
+#         print("stp")
+#         return
+    
+#     if n % 2 == 0:
+#         print("number is even",n)
+#     else:
+#         print("number is odd",n)
+#     odd_even(n  - 1)
+# odd_even(10)
+
+
+def nat(n):
+    if n == 0:
+        return 0
+    return n + nat(n-1)
+print(nat(7))
